6decc27110f368297a6712754e75f3a1274a1269
Duarte Nunes <duarte@scylladb.com>
2017-11-14 19:47:00 +0100

    util/defer: Allow functions that are non-nothrow_move_constructible
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- util/defer.hh
+++ util/defer.hh
@@ -29,12 +29,11 @@ class deferred_action {
     Func _func;
     bool _cancelled = false;
 public:
-    static_assert(std::is_nothrow_move_constructible<Func>::value, "Func(Func&&) must be noexcept");
     deferred_action(Func&& func) noexcept : _func(std::move(func)) {}
     deferred_action(deferred_action&& o) noexcept : _func(std::move(o._func)), _cancelled(o._cancelled) {
         o._cancelled = true;
     }
-    deferred_action& operator=(deferred_action&& o) noexcept {
+    deferred_action& operator=(deferred_action&& o) noexcept(std::is_nothrow_move_constructible<Func>::value) {
         if (this != &o) {
             this->~deferred_action();
             new (this) deferred_action(std::move(o));
