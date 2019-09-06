3f7849ad8cb3dec1da9c0ade32c42a48e115c38b
Duarte Nunes <duarte@vectorized.io>
2019-09-06 20:04:22 -0300

    core/future-util: Add copy_range combinator
    The copy_range combinator, inspired by boost::copy_range<>, transforms
    an input range, sequentially applying to each element the specified
    asynchronous function, and building a result container with each
    returned element.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- include/seastar/core/future-util.hh
+++ include/seastar/core/future-util.hh
@@ -1373,6 +1373,85 @@ when_all_succeed(FutureIterator begin, FutureIterator end) {
     return internal::do_when_all<result_transform>(std::move(begin), std::move(end));
 }
 
+namespace internal {
+template<typename Container, typename Iterator, typename AsyncAction>
+inline
+future<Container> copy_range(Iterator begin, Iterator end, AsyncAction action, Container c) {
+    while (begin != end) {
+        auto f = futurize_apply(action, *begin++);
+        if (!f.available()) {
+            return f.then([
+                    begin = std::move(begin),
+                    end = std::move(end),
+                    action = std::move(action),
+                    c = std::move(c)] (auto v) mutable {
+                container::inserter_for(c).insert(std::move(v));
+                return copy_range(std::move(begin), std::move(end), std::move(action), std::move(c));
+            });
+        }
+        if (f.failed()) {
+            return make_exception_future<Container>(f.get_exception());
+        }
+        container::inserter_for(c).insert(f.get0());
+        if (need_preempt()) {
+            return copy_range(std::move(begin), std::move(end), std::move(action), std::move(c));
+        }
+    }
+    return make_ready_future<Container>(std::move(c));
+}
+}
+
+/// Transforms the input range, applying to each element
+/// the specified asynchronous function, waiting for it
+/// to complete before applying the function to the next
+/// element in the input range.
+///
+/// \param begin an \c InputIterator designating the beginning of the range
+/// \param end an \c InputIterator designating the end of the range
+/// \param action a callable, taking a reference to objects from the range
+///               as a parameter, and returning a \c future<T> that contains
+///               the result of transforming an input element.
+/// \return a future resolving to the new container on success,
+/// or the first failed future if \c action failed. Order of elements is preserved
+/// where applicable (i.e., the target Container has a notion of order).
+template<typename Container, typename Iterator, typename AsyncAction>
+GCC6_CONCEPT( requires requires (AsyncAction aa, Iterator it, Container c) {
+    futurize_apply(aa, *it++);
+    requires is_future<decltype(futurize_apply(aa, *it))>::value;
+    container::inserter_for(c);
+} )
+inline
+future<Container> copy_range(Iterator begin, Iterator end, AsyncAction action) {
+    Container r;
+    using itraits = std::iterator_traits<Iterator>;
+    container::inserter_for(r).size_hint(
+        internal::iterator_range_estimate_vector_capacity(begin, end, typename itraits::iterator_category()));
+    return internal::copy_range(std::move(begin), std::move(end), std::move(action), std::move(r));
+}
+
+/// Transforms the input range, applying to each element
+/// the specified asynchronous function, waiting for it
+/// to complete before applying the function to the next
+/// element in the input range.
+///
+/// \param range a \c Range object designating input values
+/// \param action a callable, taking a reference to objects from the range
+///               as a parameter, and returning a \c future<T> that contains
+///               the result of transforming an input element.
+/// \return a future resolving to the new container on success,
+/// or the first failed future if \c action failed. Order of elements is preserved
+/// where applicable (i.e., the target Container has a notion of order).
+template<typename Container, typename Range, typename AsyncAction>
+GCC6_CONCEPT( requires requires (AsyncAction aa, Range r, Container c) {
+    futurize_apply(aa, *r.begin());
+    requires is_future<decltype(futurize_apply(aa, *r.begin()))>::value;
+    container::inserter_for(c);
+} )
+inline
+future<Container> copy_range(Range& r, AsyncAction action) {
+    return copy_range<Container>(std::begin(r), std::end(r), std::move(action));
+}
+
 }
 
 /// @}
